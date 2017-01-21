2966e9cc136494371ca8734f1b0906b7c059a5b7
L.Joel <leopard10000@gmail.com>
2017-01-21 22:13:48 +0900

    実践RoR4: 6-2 end: Sass/SCSS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # layoutを動的に切り替える仕組み
+  layout :set_layout
+
+  private
+  def set_layout
+    # コントローラー名（staff/top など）にマッチしたlayoutを返す
+    if params[:controller].match(%r{\A(staff|admin|customer)/})
+      Regexp.last_match[1]
+    else
+      'customer'
+    end
+  end
 end
