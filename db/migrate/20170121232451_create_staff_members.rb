b7f5ce0a50566c9e5e53720d47c9360f167a8252
L.Joel <leopard10000@gmail.com>
2017-01-22 08:37:46 +0900

    実践RoR4: 8-1 end: generate model, db:migrate

---
+++ db/migrate/20170121232451_create_staff_members.rb
@@ -0,0 +1,23 @@
+class CreateStaffMembers < ActiveRecord::Migration
+  def change
+    # 主キー id は暗黙的に生成される。
+    # 明示したい場合には create_table :staff_members, primary_key: 'menber_id' do |t| のようにする
+    create_table :staff_members do |t|
+      t.string :email, null: false                      # メールアドレス
+      t.string :email_for_index, null: false            # 索引用メールアドレス
+      t.string :family_name, null: false                # 姓
+      t.string :given_name, null: false                 # 名
+      t.string :family_name_kana, null: false           # 姓（カナ）
+      t.string :given_name_kana, null: false            # 名（カナ）
+      t.string :hashed_password                         # パスワード
+      t.date :start_date, null: false                   # 開始日
+      t.date :end_date                                  # 終了日
+      t.boolean :suspended, null: false, default: false # 停止フラグ
+
+      t.timestamps
+    end
+
+    add_index :staff_members, :email_for_index, unique: true
+    add_index :staff_members, [ :family_name_kana, :given_name_kana ]
+  end
+end
