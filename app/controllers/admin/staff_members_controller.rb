7589bf2debb964eee23fc5fd59ef8bd408e3deae
L.Joel <leopard10000@gmail.com>
2017-01-28 17:10:29 +0900

    実践RoR4: 11-1 管理者による職員アカウント管理機能: newアクション、form

--- app/controllers/admin/staff_members_controller.rb
+++ app/controllers/admin/staff_members_controller.rb
@@ -2,4 +2,13 @@ class Admin::StaffMembersController < Admin::Base
   def index
     @staff_members = StaffMember.order(:family_name_kana, :given_name_kana)
   end
+
+  def show
+    staff_member = StaffMember.find(params[:id])
+    redirect_to [ :edit, :admin, staff_member ]  # edit_admin_staff_member_path(id: staff_member.id) と書いた方が分かりやすいと思うが…
+  end
+
+  def new
+    @staff_member = StaffMember.new
+  end
 end
