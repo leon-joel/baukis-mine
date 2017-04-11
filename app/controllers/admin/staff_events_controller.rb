cf737a647c03c3a7fdc5ece3fe85e2104b7ef658
L.Joel <leopard10000@gmail.com>
2017-04-11 22:46:12 +0900

    実践RoR4: 14-3 モデル間の関連づけ - kaminariによるpagenation

--- app/controllers/admin/staff_events_controller.rb
+++ app/controllers/admin/staff_events_controller.rb
@@ -3,10 +3,12 @@ class Admin::StaffEventsController < Admin::Base
     if params[:staff_member_id]
       # 指定されたStaffのEvent一覧
       @staff_member = StaffMember.find(params[:staff_member_id])
-      @events = @staff_member.events.order(occurred_at: :desc)
+      @events = @staff_member.events.order(occurred_at: :desc)  # この時点ではまだQueryは実行されない。Relationオブジェクトをセットするだけ
     else
       # 全StaffのEvent一覧
-      @events = StaffEvent.order(occurred_at: :desc)
+      @events = StaffEvent.order(occurred_at: :desc)            # 同上
     end
+
+    @events = @events.page(params[:page])                       # ここでもまだQueryは実行されず、Relationオブジェクトに条件を追加しているだけ
   end
 end
